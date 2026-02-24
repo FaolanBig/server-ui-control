c746c2a9f4d13eab41d67a6232414664a9230813
faolanbig <faolan.big@web.de>
2026-02-24 08:52:22 +0100

    update

--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -4,13 +4,37 @@
 # 4 "/home/fb/repos/server-ui-control/server-ui-control.ino" 2
 # 5 "/home/fb/repos/server-ui-control/server-ui-control.ino" 2
 # 6 "/home/fb/repos/server-ui-control/server-ui-control.ino" 2
-# 22 "/home/fb/repos/server-ui-control/server-ui-control.ino"
+# 25 "/home/fb/repos/server-ui-control/server-ui-control.ino"
 unsigned int colors[] = {0x0000, 0x001F, 0xF800, 0x07E0, 0x07FF, 0xF81F, 0xFFE0, 0xFFFF};
 
 MCUFRIEND_kbv tft;
 
+const int XP = 8, XM = A2, YP = A3, YM = 9; //ID=0x9341
+const int TS_LEFT = 136, TS_RT = 907, TS_TOP = 942, TS_BOT = 139;
+
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+
+Adafruit_GFX_Button on_btn, off_btn;
+
+int pixel_x, pixel_y; //Touch_getXY() updates global vars
+
 int width = 320;
 int height = 480;
+
+bool Touch_getXY(void)
+{
+    TSPoint p = ts.getPoint();
+    pinMode(YP, 0x1); //restore shared pins
+    pinMode(XM, 0x1);
+    digitalWrite(YP, 0x1); //because TFT control pins
+    digitalWrite(XM, 0x1);
+    bool pressed = (p.z > 200 && p.z < 1000);
+    if (pressed) {
+        pixel_x = map(p.x, TS_LEFT, TS_RT, 0, tft.width()); //.kbv makes sense to me
+        pixel_y = map(p.y, TS_TOP, TS_BOT, 0, tft.height());
+    }
+    return pressed;
+}
 
 void setup()
 {
--- build/sketch/server-ui-control.ino.cpp
+++ build/sketch/server-ui-control.ino.cpp
@@ -21,18 +21,47 @@
 #define YELLOW 0xFFE0
 #define WHITE 0xFFFF
 
+#define MINPRESSURE 200
+#define MAXPRESSURE 1000
+
 unsigned int colors[] = {BLACK, BLUE, RED, GREEN, CYAN, MAGENTA, YELLOW, WHITE};
 
 MCUFRIEND_kbv tft;
 
+const int XP = 8, XM = A2, YP = A3, YM = 9; //ID=0x9341
+const int TS_LEFT = 136, TS_RT = 907, TS_TOP = 942, TS_BOT = 139;
+
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+
+Adafruit_GFX_Button on_btn, off_btn;
+
+int pixel_x, pixel_y;     //Touch_getXY() updates global vars
+
 int width = 320;
 int height = 480;
 
-#line 29 "/home/fb/repos/server-ui-control/server-ui-control.ino"
-void setup();
 #line 41 "/home/fb/repos/server-ui-control/server-ui-control.ino"
+bool Touch_getXY(void);
+#line 56 "/home/fb/repos/server-ui-control/server-ui-control.ino"
+void setup();
+#line 68 "/home/fb/repos/server-ui-control/server-ui-control.ino"
 void loop();
-#line 29 "/home/fb/repos/server-ui-control/server-ui-control.ino"
+#line 41 "/home/fb/repos/server-ui-control/server-ui-control.ino"
+bool Touch_getXY(void)
+{
+    TSPoint p = ts.getPoint();
+    pinMode(YP, OUTPUT);      //restore shared pins
+    pinMode(XM, OUTPUT);
+    digitalWrite(YP, HIGH);   //because TFT control pins
+    digitalWrite(XM, HIGH);
+    bool pressed = (p.z > MINPRESSURE && p.z < MAXPRESSURE);
+    if (pressed) {
+        pixel_x = map(p.x, TS_LEFT, TS_RT, 0, tft.width()); //.kbv makes sense to me
+        pixel_y = map(p.y, TS_TOP, TS_BOT, 0, tft.height());
+    }
+    return pressed;
+}
+
 void setup()
 {
 	Serial.begin(9600);
